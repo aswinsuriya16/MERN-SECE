a4b69ee4ca0460df707950fc4972d52d2ff73739
aswinsuriya16 <aswinsuriya16@gmail.com>
2025-01-10 09:21:18 +0530

    4th day commit

---
+++ datatype.js
@@ -0,0 +1,21 @@
+a = "SECE"
+console.log(a)
+b = true
+console.log(b)
+console.log(typeof(a))
+array = ['a',10,true,[20,30],"Sri Eshwar"]
+console.log(array)
+
+//obj1 = {}
+//console.log(obj1)
+//obj1["dept"] = "cse"
+//obj1['sec'] = "A"
+//console.log(obj1)
+
+//obj2 = new Object()
+//console.log(obj2)
+//obj2.count = 70
+//obj2.abs = 1
+//totalPresentCount = obj2.count - obj2.abs
+//console.log("Present", totalPresentCount)
+//console.log(obj2)
