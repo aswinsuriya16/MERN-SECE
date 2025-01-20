a0f970fbfefe91b858514f0440980d72e415c7ea
aswinsuriya16 <aswinsuriya16@gmail.com>
2025-01-20 09:27:04 +0530

    Day 5

---
+++ looping.js
@@ -0,0 +1,20 @@
+//for in loop
+
+var a = [10,20,30]
+var b = {"count":70, "abs":1}
+for(let i in a){
+    console.log(i)
+}
+for(let i in b){
+    console.log(i,b[i])
+}
+set = new Set(["hello",1.2,"SECE","44",2,"hello","SECE",5])
+for(let i in set){
+    console.log("loop is running")
+    console.log(i)
+}
+
+//for of
+ /*for (let i of b){
+    console.log(i)
+ }*/
